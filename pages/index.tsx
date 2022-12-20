d986f42acec9d51dc8e237b3c9224a49d71503e3
evnwttn <evnwttn@gmail.com>
2022-12-20 20:28:41 -0330

    mui/material / types

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,27 +6,8 @@ export default function Home() {
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <meta name="theme-color" content="#272727" />
-        <meta
-          name="description"
-          content="a visual album management toolkit designed to track & monitor progress, foster collaboration, & aid in the smashing of goals."
-        />
-        <meta property="og:title" content="ao | album organizer" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:image"
-          content="https://i.ibb.co/RQTq9q5/aoapp.png"
-        />
-        <meta property="og:url" content="https://evnwttn.github.io/ao/" />
-        <meta
-          property="og:description"
-          content="a visual album management toolkit designed to track & monitor progress, foster collaboration, & aid in the smashing of goals."
-        />
 
         <title>ao | album organizer</title>
-
-        {/* <link rel="icon" href="favicon.ico" />
-        <link rel="apple-touch-icon" href="logo192.png" />
-        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" /> */}
       </Head>
       <>ao</>
     </>
