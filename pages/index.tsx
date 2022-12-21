2dd39fa775d0aab3931cfa3c4003ab66f28298be
evnwttn <evnwttn@gmail.com>
2022-12-20 21:01:42 -0330

    :pager:

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,8 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <meta name="theme-color" content="#272727" />
 
+        <link rel="stylesheet" href="https://use.typekit.net/nnr5nkk.css" />
+
         <title>ao | album organizer</title>
       </Head>
       <>ao</>
