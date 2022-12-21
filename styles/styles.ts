e69307613ec8bedfb75b269e1d8507ec67029abf
evnwttn <evnwttn@gmail.com>
2022-12-20 21:13:41 -0330

    themin

---
+++ styles/styles.ts
@@ -0,0 +1,9 @@
+export const palette = {
+  _blue: "#7284A8",
+  _yellow: "#F2BF80",
+  _red: "#D93D1A",
+  _black: "#272727",
+  _darkgrey: "#707070",
+  _grey: "#2D2D2D",
+  _white: "#F3EFE0",
+};
