8d5f9e79953ab47b279e1f189229179e7fc03a8e
evnwttn <evnwttn@gmail.com>
2022-12-29 14:26:06 -0330

    Revert "Deploying to main from @ evnwttn/ao2@26622b99bb3addfcd5114254fd8fad3d2bb82f34 🚀"
    This reverts commit 97e86ba98b0728969a29d32acd7dfa93e7817bdd.

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta name="theme-color" content="#272727" />
+
+        <link rel="stylesheet" href="https://use.typekit.net/nnr5nkk.css" />
+
+        <title>ao | album organizer</title>
+      </Head>
+      <>ao</>
+    </>
+  );
+}
