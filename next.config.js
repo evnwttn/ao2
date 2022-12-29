92fe99a39f846d0df37fab4491e93179d5a7c7ea
evnwttn <evnwttn@gmail.com>
2022-12-29 12:46:24 -0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 const repo = "ao2";
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}`;
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
 
 module.exports = {
   assetPrefix: assetPrefix,
   basePath: basePath,
+  //   images: {
+  //     loader: "imgix",
+  //     path: 'the "domain" of your Imigix source',
+  //   },
 };
