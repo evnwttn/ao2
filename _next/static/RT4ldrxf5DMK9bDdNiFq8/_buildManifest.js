781653457c8cb7244806b83eea72399030ddbdd2
evnwttn <evnwttn@gmail.com>
2023-01-03 19:47:16 +0000

    Deploying to gh-pages from @ evnwttn/ao2@ec17f8c6bfeb18ffa8ef34867119d240afb2a3f5 🚀

--- _next/static/RT4ldrxf5DMK9bDdNiFq8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/754-e279b66d657ad79e.js","static/chunks/pages/index-88b4bb931b0158f3.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/f7cKclrU9rDiMnGpWmWAk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/754-e279b66d657ad79e.js","static/chunks/pages/index-dba4e5802bc88125.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
