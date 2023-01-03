bc8fbb762ea521c281b824a68dd0fbfca53264c6
evnwttn <evnwttn@gmail.com>
2023-01-03 15:19:40 -0330

    temp remove head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
-import Head from "next/head";
+// import Head from "next/head";
 import { Splash } from "../components/index";
 
 export default function Home() {
   return (
     <>
-      <Head>
+      {/* <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <meta name="theme-color" content="#272727" />
 
         <link rel="stylesheet" href="https://use.typekit.net/nnr5nkk.css" />
 
         <title>ao | album organizer</title>
-      </Head>
+      </Head> */}
       <Splash />
     </>
   );
