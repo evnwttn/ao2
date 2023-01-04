9744536fa44f75dddfb69f118da70807953d927e
evnwttn <evnwttn@gmail.com>
2023-01-04 13:09:54 -0330

    Create grid.tsx

---
+++ pages/grid.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function Grid() {
+  return (
+    <Head>
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+      <meta name="theme-color" content="#272727" />
+      <title>ao | album organizer</title>
+    </Head>
+  );
+}
