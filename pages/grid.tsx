82a41c91db2695db34155d915ec860f43fe62c98
evnwttn <evnwttn@gmail.com>
2023-01-04 13:19:46 -0330

    Update grid.tsx

--- pages/grid.tsx
+++ pages/grid.tsx
@@ -1,11 +1,15 @@
+import React from "react";
 import Head from "next/head";
 
 export default function Grid() {
   return (
-    <Head>
-      <meta name="viewport" content="initial-scale=1, width=device-width" />
-      <meta name="theme-color" content="#272727" />
-      <title>ao | album organizer</title>
-    </Head>
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta name="theme-color" content="#272727" />
+        <title>ao | album organizer</title>
+      </Head>
+      <>Hello</>
+    </>
   );
 }
