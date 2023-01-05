d026be88b10be28f1735166d5ddf4ea7cef65faf
evnwttn <evnwttn@gmail.com>
2023-01-05 09:03:05 -0330

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import React from "react";
 import "../styles/styles.css";
+import React from "react";
+import { GridDataProvider } from "../components/contexts/gridDataContext";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <GridDataProvider>
+      <Component {...pageProps} />
+    </GridDataProvider>
+  );
 }
