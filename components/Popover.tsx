96a4fa4b795b4b182f6cbf6fcf87ab24806991fb
evnwttn <evnwttn@gmail.com>
2023-01-06 08:56:09 -0330

    Update Popover.tsx

--- components/Popover.tsx
+++ components/Popover.tsx
@@ -1,57 +1,53 @@
 import React, { useState, useEffect } from "react";
-// import { Box, Tooltip } from "@mui/material/";
-// import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
-// import { ColorWheel, AOCommentBase } from "./index";
-// import { wheelSx } from "../assets/theme";
+import { Box, Tooltip } from "@mui/material/";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
+import { ColorWheel, CommentBase } from "./index";
+import { inline } from "../styles";
 
-export const Popover = () => {
-  return <></>;
+export const Popover = ({
+  cell,
+  cellComment,
+  handleClick,
+  register,
+  handleSubmit,
+  onSubmit,
+}) => {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const [commentOpen, setCommentOpen] = useState("1");
+
+  const handleClose = () => {
+    setIsPopoverOpen(false);
+    setCommentOpen("1");
+  };
+
+  useEffect(() => {
+    commentOpen !== "1" ? setIsPopoverOpen(true) : setIsPopoverOpen(false);
+  }, [commentOpen]);
+
+  useEffect(() => {
+    setIsPopoverOpen(false);
+  }, [cellComment, setIsPopoverOpen]);
+
+  return (
+    <>
+      <CommentBase
+        cell={cell}
+        cellComment={cellComment}
+        open={isPopoverOpen}
+        handleClose={handleClose}
+        register={register}
+        handleSubmit={handleSubmit}
+        onSubmit={onSubmit}
+      />
+      <ColorWheel
+        handleClick={(color) => handleClick(color)}
+        openComment={() => setCommentOpen("2")}
+      />
+      <Box sx={inline.wheelSx.remove}>
+        <Tooltip title="Cancel Task" placement="right-end">
+          <RemoveCircleOutlineIcon onClick={() => handleClick("transparent")} />
+        </Tooltip>
+      </Box>
+    </>
+  );
 };
-
-// export const Popover = ({
-//   cell,
-//   cellComment,
-//   handleClick,
-//   register,
-//   handleSubmit,
-//   onSubmit,
-// }) => {
-//   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
-//   const [commentOpen, setCommentOpen] = useState("1");
-
-//   const handleClose = () => {
-//     setIsPopoverOpen(false);
-//     setCommentOpen("1");
-//   };
-
-//   useEffect(() => {
-//     commentOpen !== "1" ? setIsPopoverOpen(true) : setIsPopoverOpen(false);
-//   }, [commentOpen]);
-
-//   useEffect(() => {
-//     setIsPopoverOpen(false);
-//   }, [cellComment, setIsPopoverOpen]);
-
-//   return (
-//     <>
-//       <AOCommentBase
-//         cell={cell}
-//         cellComment={cellComment}
-//         open={isPopoverOpen}
-//         handleClose={handleClose}
-//         register={register}
-//         handleSubmit={handleSubmit}
-//         onSubmit={onSubmit}
-//       />
-//       <ColorWheel
-//         handleClick={(color) => handleClick(color)}
-//         openComment={() => setCommentOpen("2")}
-//       />
-//       <Box sx={wheelSx.remove}>
-//         <Tooltip title="Cancel Task" placement="right-end">
-//           <RemoveCircleOutlineIcon onClick={() => handleClick("transparent")} />
-//         </Tooltip>
-//       </Box>
-//     </>
-//   );
-// };
