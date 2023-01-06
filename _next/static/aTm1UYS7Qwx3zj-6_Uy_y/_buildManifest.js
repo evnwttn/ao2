2431109cbc811819b79a381d0cc6087840ccdc67
evnwttn <evnwttn@gmail.com>
2023-01-06 13:34:47 +0000

    Deploying to gh-pages from @ evnwttn/ao2@f2a82fad0ccef51db576b7ca9b09ab600de76e1b 🚀

---
+++ _next/static/aTm1UYS7Qwx3zj-6_Uy_y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/163-0b711c77dfaa5300.js","static/chunks/pages/index-b24c10d924b94b99.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
