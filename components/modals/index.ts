b00ed44475d9640962dc19c9488f34b2c7e4ad61
evnwttn <evnwttn@gmail.com>
2023-01-06 08:46:05 -0330

    Update index.ts

--- components/modals/index.ts
+++ components/modals/index.ts
@@ -1,4 +1,5 @@
 export { AboutContent } from "./AboutContent";
+export { CommentContent } from "./CommentContent";
 export { ContactContent } from "./ContactContent";
 export { LoadContent } from "./LoadContent";
 export { NewContent } from "./NewContent";
