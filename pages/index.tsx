10e5d5aff2ec66aef760953d8ca0385ea62a4f8b
evnwttn <evnwttn@gmail.com>
2023-01-11 16:35:01 -0330

    bump

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,9 +26,29 @@ export default function Home() {
   return (
     <>
       <Head>
+        <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <meta name="theme-color" content="#272727" />
+        <meta
+          name="description"
+          content="the visual album management toolkit designed to track & monitor progress, foster collaboration, & aid in the smashing of goals."
+        />
         <title>ao | album organizer</title>
+
+        <meta property="og:title" content="ao | album organizer" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://i.ibb.co/RQTq9q5/aoapp.png"
+        />
+        <meta property="og:url" content="https://ao-demo.vercel.app/" />
+        <meta
+          property="og:description"
+          content="the visual album management toolkit designed to track & monitor progress, foster collaboration, & aid in the smashing of goals."
+        />
+
+        <link rel="icon" href="../public/imagesfavicon.ico" />
+        <link rel="apple-touch-icon" href="../public/imageslogo192.png" />
       </Head>
       {gridData ? (
         <AOGrid
